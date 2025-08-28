d1e930d5782cd42d6af8a372b9757c97c298c6f3
elliotdwadwfa <ellioternstemeyer@gmail.com>
2025-08-28 09:42:10 -0500

    inital commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/**
+ * Basic hello world program
+ */
+
+ int main(int argc, char **argv) {
+
+  printf("Elliot Ernstmeyer, Computer Engineering!\n");
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
+
